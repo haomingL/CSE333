809b5370136bc90bcadd74edbb1fe867e746a4f7
haomingL <haoming9502@gmail.com>
2015-12-03 16:23:34 -0800

    add ex09

---
+++ ex09/TokenCounter.h
@@ -0,0 +1,28 @@
+#ifndef TOKENCOUNTER_H
+#define TOKENCOUNTER_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+class TokenCounter {
+ public:
+  // Takes a vector of tokens (strings) with possible duplicates
+  TokenCounter(const std::vector<std::string> &tokenCollection);
+  
+  // We declare the destructor in case some implementation requires one
+  ~TokenCounter();
+  
+  // Returns the N most frequently occuring tokens in the constructor arg vector
+  std::vector<std::pair<std::string,int>> getTop(int N);
+
+ private:
+  // whatever you want here
+ 	std::map<std::string, int> words;
+ 	std::vector<std::pair<std::string,int>> sorted_words;
+ 	bool sorted;
+
+ 	std::vector<std::pair<std::string,int>> sort();
+};
+
+#endif // TOKENCOUNTER_H
